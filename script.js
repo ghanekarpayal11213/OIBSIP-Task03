89297532c3bd57df7fcb33423febf163f20dd7b5
ghanekarpayal11213 <126890237+ghanekarpayal11213@users.noreply.github.com>
2023-09-14 19:59:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const temperature =()=> {
+    const inputTemp=document.getElementById('temp').value;
+    const tempSelected=document.getElementById('temp_diff');
+    const valueTemp =temp_diff.options[tempSelected.
+    selectedIndex].value;
+
+    //cal to feh
+ const celToFah =(cel) => {
+    let fahrenheit =((cel *9/5)+32).toFixed(1);
+    return fahrenheit;
+ }
+ //fah to cel
+ const fahTocel=(fah)=>{
+    let celsius=((fah-32)*5/9).toFixed(1);
+    return celsius;
+ }
+if(valueTemp=='cel'){
+    document.getElementById("result").innerHTML=celToFah
+    (inputTemp)+"&#176; Fahrenheit";
+}
+else{
+    document.getElementById("result").innerHTML=fahTocel
+    (inputTemp)+"&#176; Celsius";
+}
+}
